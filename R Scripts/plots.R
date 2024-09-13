d7e6908f9ccd69e8635582d5f6345ac2839ed4c8
bezerra-ag <bezerraag.acad@gmail.com>
2024-09-13 14:02:12 -0300

    new graphs

--- R Scripts/plots.R
+++ R Scripts/plots.R
@@ -3,16 +3,33 @@ setwd   ("../R Scripts")
 source  ("data.R")
 require ("ggplot2")
 
-#Count by Year - Line
+#Count by Gender - Line
+ggplot(d[d$Gender    != "Não divulgável",],
+       aes(x          = Year,
+           col        = Gender)) +
+  geom_line(stat      = "count",
+            alpha     = 0.7,
+            linewidth = 1.5,
+            lineend   = "round",
+            arrow     = arrow(),
+            aes(group = Gender)) +
+  labs(
+    title = "Candidaturas por Gênero",
+    x     = "Ano",
+    y     = "Número de Candidaturas",
+    col   = "Gênero"
+  )
+
+#Count by Spectrum - Line
 ggplot(d[d$Gender  != "Não divulgável",],
        aes(x        = Year,
            col      = Spectrum)) +
   
-  geom_line(stat = "count",
-            alpha = 0.8,
+  geom_line(stat      = "count",
+            alpha     = 0.7,
             linewidth = 1.5,
-            lineend = "round",
-            arrow = arrow(),
+            lineend   = "round",
+            arrow     = arrow(),
             aes(group = Spectrum)) +
   facet_grid(Gender ~ .) +
   
@@ -20,5 +37,22 @@ ggplot(d[d$Gender  != "Não divulgável",],
     title = "Candidaturas por Posição no Espectro Político",
     x     = "Ano",
     y     = "Número de Candidaturas",
-    fill  = "Posição no Espectro Político"
+    col   = "Posição no Espectro Político"
+  )
+
+#Count by the Liberal Party - Line
+ggplot(d[d$Gender    != "Não divulgável" & (d$Party == "PL" | d$Party == "PR"),],
+       aes(x          = Year,
+           col        = Gender)) +
+  geom_line(stat      = "count",
+            alpha     = 0.7,
+            linewidth = 1.5,
+            lineend   = "round",
+            arrow     = arrow(),
+            aes(group = Gender)) +
+  labs(
+    title = "Candidaturas pelo Partido Liberal",
+    x     = "Ano",
+    y     = "Número de Candidaturas",
+    col   = "Gênero"
   )
