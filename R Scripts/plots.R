2ffed08df162acd87a1273863dc028b0674fd12b
bezerra-ag <bezerraag.acad@gmail.com>
2024-09-18 22:48:05 -0300

    plot optimization

--- R Scripts/plots.R
+++ R Scripts/plots.R
@@ -3,56 +3,23 @@ setwd   ("../R Scripts")
 source  ("data.R")
 require ("ggplot2")
 
-#Count by Gender - Line
-ggplot(d[d$Gender    != "Não divulgável",],
-       aes(x          = Year,
-           col        = Gender)) +
-  geom_line(stat      = "count",
-            alpha     = 0.7,
-            linewidth = 1.5,
-            lineend   = "round",
-            arrow     = arrow(),
-            aes(group = Gender)) +
-  labs(
-    title = "Candidaturas por Gênero",
-    x     = "Ano",
-    y     = "Número de Candidaturas",
-    col   = "Gênero"
-  )
+#Arrows on Year x Count for 'key', facet by Gender
+p <- function(key, name){
+  ggplot(d[d$Gender != "Não divulgável",], aes(x = Year, col = {{key}})) +
+    geom_line(stat = "count", alpha = 0.7, linewidth = 1.5, lineend = "round", arrow = arrow(), aes(group = {{key}})) +
+    facet_grid(Gender ~ .) +
+    labs(title = paste("Candidaturas por",name), x = "Ano", y = "Número de Candidaturas", col = name)
+}
 
-#Count by Spectrum - Line
-ggplot(d[d$Gender  != "Não divulgável",],
-       aes(x        = Year,
-           col      = Spectrum)) +
-  
-  geom_line(stat      = "count",
-            alpha     = 0.7,
-            linewidth = 1.5,
-            lineend   = "round",
-            arrow     = arrow(),
-            aes(group = Spectrum)) +
-  facet_grid(Gender ~ .) +
-  
-  labs(
-    title = "Candidaturas por Posição no Espectro Político",
-    x     = "Ano",
-    y     = "Número de Candidaturas",
-    col   = "Posição no Espectro Político"
-  )
+p(Region,             "Região")
+p(Age,                "Faixa Etária")
+p(Civil.State,        "Estado Civil")
+p(Education,          "Grau de Instrução")
+p(Reelection,         "Reeleição")
 
-#Count by the Liberal Party - Line
-ggplot(d[d$Gender    != "Não divulgável" & (d$Party == "PL" | d$Party == "PR"),],
-       aes(x          = Year,
-           col        = Gender)) +
-  geom_line(stat      = "count",
-            alpha     = 0.7,
-            linewidth = 1.5,
-            lineend   = "round",
-            arrow     = arrow(),
-            aes(group = Gender)) +
-  labs(
-    title = "Candidaturas pelo Partido Liberal",
-    x     = "Ano",
-    y     = "Número de Candidaturas",
-    col   = "Gênero"
-  )
+ggplot(d[d$Gender != "Não divulgável",], aes(x = Party, fill = Gender)) +
+  geom_bar(position = "dodge") +
+  facet_grid(Year ~ Spectrum, scales = "free_x") +
+  labs(title = "Candidaturas por Partido", x = "Partido", y = "Número de Candidaturas", fill = "Gênero") +
+  theme(axis.text.x = element_text(angle = 90))
+
